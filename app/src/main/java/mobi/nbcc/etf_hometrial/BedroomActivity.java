fd6aa18e604a2f4de86e3cfb6aef6dc330143b5a
Nigel Martin <nigelgmartin@live.ca>
2015-02-26 22:10:23 -0400

    initial commit

---
+++ app/src/main/java/mobi/nbcc/etf_hometrial/BedroomActivity.java
@@ -0,0 +1,15 @@
+package mobi.nbcc.etf_hometrial;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by tommy-the-cat on 1/31/2015.
+ */
+public class BedroomActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bedroom);
+    }
+}
